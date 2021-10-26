78c9a05d39d962bbdd10994f09076f1627d76f51
tejas-niturkar <tejas.niturkar@gmail.com>
2021-10-26 06:34:23 +0000

    updated java 7 recipe

--- chef-repo/cookbooks/java/recipes/default.rb
+++ chef-repo/cookbooks/java/recipes/default.rb
@@ -3,3 +3,18 @@
 # Recipe:: default
 #
 # Copyright:: 2021, The Authors, All Rights Reserved.
+
+name "java"
+description "Install Oracle Java"
+default_attributes(
+  "java" => {
+    "install_flavor" => "oracle",
+    "jdk_version" => "7",
+    "oracle" => {
+      "accept_oracle_download_terms" => true
+    }
+  }
+)
+run_list(
+  "recipe[java]"
+)
